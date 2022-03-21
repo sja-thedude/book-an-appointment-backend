4cfb15eda9c13ea68ab9dd4ae73a0a8d434871e4
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-03-21 21:11:58 +0300

    Add Tests for User, doctor

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,1 @@
-class Reservation < ApplicationRecord
-  validates :reservation_time, presence: { message: 'You must fill in your name' }
-
-  belongs_to :user
-  belongs_to :doctor
-end
+#reservation model
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,1 +1,17 @@
-# address test
+require 'rails_helper'
+
+RSpec.describe Address, type: :model do
+  subject do
+    Address.new(city: 'London', state: 'Orleans', country:
+    'England', street: '9 rue de Bourge 45000')
+  end
+  it 'a Address should has a valid state, city, country and street' do
+    expect(subject).to be_valid
+    subject.city = ''
+    expect(subject).to_not be_valid
+    subject.country = nil
+    expect(subject).to_not be_valid
+    subject.street = nil
+    expect(subject).to_not be_valid
+  end
+end
