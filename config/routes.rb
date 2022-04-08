495e2aba7883ac09e5572f07c81240edd1162bb1
ShahierNashaat <shahiernashaat97@gmail.com>
2022-04-08 10:28:07 +0200

    Remove comments from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
-  # devise_for :users, defaults: { format: :json },
-  #            controllers: {
-  #                sessions: 'users/sessions',
-  #                registrations: 'users/registrations'
-  #            }
-  # get '/member-data', to: 'members#show'
-
-
-  
   resources :addresses
   resources :doctors
   resources :users, only: %i[index authenticate destroy] do
     resources :reservations, only: %i[index show create destroy]
   end
-  # Defines the root path route ("/")
-  # root "doctors#index"
 end
